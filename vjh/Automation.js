a536ed808fe3bacc0ed0b23131af520d5e3c2802
B. Gagan Chandra <89629223+Gagan1708@users.noreply.github.com>
2021-10-30 11:25:39 +0530

    Add files via upload

---
+++ vjh/Automation.js
@@ -0,0 +1,9 @@
+function openNav() {
+    document.getElementById("Sidebar").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("Sidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
